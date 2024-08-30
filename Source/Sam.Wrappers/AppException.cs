bfa91399e6ad3f3e1e96353785a5c72b9f19c6da
saman azadi <samanazadi1996@gmail.com>
2024-08-30 18:14:23 +0330

    clean code

--- Source/Sam.Wrappers/AppException.cs
+++ Source/Sam.Wrappers/AppException.cs
@@ -6,11 +6,10 @@ namespace Sam.Wrappers
     public class AppException : Exception
     {
         public readonly Error Error;
-        public AppException(Error error) => Error = error;
+        public AppException(Error error)
+           => Error = error;
 
         public static void Throw(ErrorCode errorCode, string? description = null, string? fieldName = null)
-        {
-            throw new AppException(new Error(errorCode, description, fieldName));
-        }
+            => throw new AppException(new Error(errorCode, description, fieldName));
     }
 }
